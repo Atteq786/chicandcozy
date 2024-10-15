93be931b5724e808b167427e7fb9b64d858717b1
MuhammadAtteq <muhammadatteq7866@gmail.com>
2024-10-15 20:53:42 +0500

    main

--- public/js/home.js
+++ public/js/home.js
@@ -2,32 +2,29 @@
 
 const navbar = document.querySelector(".navbar");
 window.addEventListener(`scroll`, () => {
-    if (scrollY >= 180) {
-        navbar.classList.add("bg");
-    }
-    else {
-        navbar.classList.remove("bg");
-    }
+	if (scrollY >= 180) {
+		navbar.classList.add("bg");
+	} else {
+		navbar.classList.remove("bg");
+	}
 });
 
 //Image Collage
 
-const images = document.querySelectorAll('.collage-image');
+const images = document.querySelectorAll(".collage-image");
 
-images.forEach(image => {
-    image.addEventListener('mouseover', () => {
-        images.forEach(img => {
-            if (img !== image) {
-                img.style.filter = 'blur(10px)';
-            }
-        });
-    });
+images.forEach((image) => {
+	image.addEventListener("mouseover", () => {
+		images.forEach((img) => {
+			if (img !== image) {
+				img.style.filter = "blur(10px)";
+			}
+		});
+	});
 
-    image.addEventListener('mouseout', () => {
-        images.forEach(img => {
-            img.style.filter = 'none';
-        });
-    });
+	image.addEventListener("mouseout", () => {
+		images.forEach((img) => {
+			img.style.filter = "none";
+		});
+	});
 });
-
-
