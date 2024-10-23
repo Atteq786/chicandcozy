2d3a234f964bd5ba7549f800dc608f703292f335
MuhammadAtteq <muhammadatteq7866@gmail.com>
2024-10-23 22:00:12 +0500

    initial commit

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 const app = express();
 
 //middlewares
+app.use(express.static("public"));
 app.use(express.static("public/css"));
 app.use(express.json());  //enables form sharing
 
@@ -16,21 +17,22 @@ app.get(`/`, (req, res) => {
     res.sendFile("index.html", { root: "public" })
 });
 
+//Signup 
+app.get(`/signup`, (req, res) => {
+    res.sendFile("signup.html", { root: "public" })
+});
 
-app.get(`404`, (req, res) => {
+
+app.get(`/404`, (req, res) => {
     res.sendFile("404.html", { root: "public" })
 });
 
 app.use((req, res, next) => {
-    if (req.url !== '/404') {
-        res.redirect("/404");
-    } else {
-        next(); // Allow the next middleware to handle the /404 request
-    }
+    res.status(404).sendFile("404.html", { root: "public" });
 });
 
 let port = 8080;
 app.listen(port, () => {
-    console.log("listening to the port")
+    console.log(`listening to the port ${port}`);
     
 });
